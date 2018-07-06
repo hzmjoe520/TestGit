57d47542a4471a511ec87ad4134eef9a484aa962
eclipse_user <eclipse_email>
2018-07-06 15:50:57 +0800

    third commit

--- src/main/java/com/hzm/git/Happy.java
+++ src/main/java/com/hzm/git/Happy.java
@@ -1,5 +1,9 @@
 package com.hzm.git;
 
 public class Happy {
-      
+       public static void main(String[] args) {
+    	   
+	     System.out.println("hello world");	
+	     
+	 }
 }
--- src/test/java/com/hzm/test/GoodTest.java
+++ src/test/java/com/hzm/test/GoodTest.java
@@ -1,5 +1,5 @@
 package com.hzm.test;
 
 public class GoodTest {
-      
+       
 }
